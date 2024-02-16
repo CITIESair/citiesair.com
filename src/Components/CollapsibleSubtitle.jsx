c3e4384b1f83122da838e5ffe932c3bbb3d01162
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2024-02-16 10:21:47 +0200

    minor fixes for the collapsible subtitles

--- src/Components/CollapsibleSubtitle.jsx
+++ src/Components/CollapsibleSubtitle.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
@@ -6,32 +6,25 @@ import parse from 'html-react-parser';
 import { useMediaQuery, useTheme } from '@mui/material';
 import { replacePlainHTMLWithMuiComponents } from '../Utils/Utils';
 
-function CollapsibleSubtitle({ text, wordLimit = 50, reference }) {
+function CollapsibleSubtitle({ text, wordLimit = 40, reference }) {
   const [isExpanded, setIsExpanded] = useState(false);
-
-  // React to changes in screen width
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm')); // 'sm' means "small and below"
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const subtitleRef = useRef(null);
 
   const words = text.split(' ');
 
   // If not on mobile or words are within limit, just display the full text
-  if (!isMobile || words.length <= wordLimit) {
+  if (!isMobile || words.length / wordLimit <= 1.4) {
     return (
-      <>
-        {text &&
-          (
-            <Typography>
-              {parse(text, { replace: replacePlainHTMLWithMuiComponents })}
-            </Typography>
-          )}
-        {reference
-          && (
-            <Typography variant="caption" color="text.secondary">
-              {parse(reference, { replace: replacePlainHTMLWithMuiComponents })}
-            </Typography>
-          )}
-      </>
+      <Box>
+        <Typography>{parse(text, { replace: replacePlainHTMLWithMuiComponents })}</Typography>
+        {reference && (
+          <Typography variant="caption" color="text.secondary">
+            {parse(reference, { replace: replacePlainHTMLWithMuiComponents })}
+          </Typography>
+        )}
+      </Box>
     );
   }
 
@@ -42,39 +35,54 @@ function CollapsibleSubtitle({ text, wordLimit = 50, reference }) {
     displayText = displayText.slice(0, lastFullStop + 1);
   }
 
+  // Do not collapse if the user clicks on the chart associated with the subtitle
+  const isClickWithinSharedParent = (event) => {
+    let { target } = event;
+    // .MuiContainer-root - Container for both the subtitle and the chart
+    const subtitleParent = subtitleRef.current.closest('.MuiContainer-root');
+    // Elements used to detect if the click is on a chart
+    const chartElements = new Set(['rect', 'path', 'svg', 'circle']);
+
+    while (target != null) {
+      // Check if a chart is clicked
+      if (chartElements.has(target.tagName)) {
+        // If the chart element and the subtitle element share the same container
+        // it means that the click is on the chart associated with the subtitle
+        // As such, we should not collapse the subtitle
+        const targetParent = target.closest('.MuiContainer-root');
+        return targetParent === subtitleParent;
+      }
+      target = target.parentNode; // Move up the DOM tree
+    }
+    return false;
+  };
+
+  // Custom onClickAway handler
+  const handleOnClickAway = (event) => {
+    if (!isClickWithinSharedParent(event)) {
+      setIsExpanded(false);
+    }
+  };
+
   return (
-    <ClickAwayListener onClickAway={() => setIsExpanded(false)}>
-      <Box onClick={() => setIsExpanded(!isExpanded)} style={{ cursor: 'pointer' }}>
-        {isExpanded
-          ? (
-            <>
-              <Typography>{parse(text, { replace: replacePlainHTMLWithMuiComponents })}</Typography>
-              {reference && <Typography variant="caption" color="text.secondary">{parse(reference, { replace: replacePlainHTMLWithMuiComponents })}</Typography>}
-            </>
-          )
-          : (
-            <>
-              <Typography
-                display="inline"
-              >
-                {parse(displayText, { replace: replacePlainHTMLWithMuiComponents })}
-                ...
-              </Typography>
-              <Typography
-                display="inline"
-                color="text.primary"
-                fontWeight="500"
-                sx={{
-                  ml: 0.5,
-                  '& :hover': {
-                    textDecoration: 'underline'
-                  }
-                }}
-              >
-                See more
-              </Typography>
-            </>
-          )}
+    <ClickAwayListener onClickAway={handleOnClickAway}>
+      <Box ref={subtitleRef} onClick={() => setIsExpanded(!isExpanded)} style={{ cursor: 'pointer' }}>
+        {isExpanded ? (
+          <>
+            <Typography>{parse(text, { replace: replacePlainHTMLWithMuiComponents })}</Typography>
+            {reference && <Typography variant="caption" color="text.secondary">{parse(reference, { replace: replacePlainHTMLWithMuiComponents })}</Typography>}
+          </>
+        ) : (
+          <>
+            <Typography display="inline">
+              {parse(displayText, { replace: replacePlainHTMLWithMuiComponents })}
+              ...
+            </Typography>
+            <Typography display="inline" color="text.primary" fontWeight="500" sx={{ ml: 0.5, '& :hover': { textDecoration: 'underline' } }}>
+              See more
+            </Typography>
+          </>
+        )}
       </Box>
     </ClickAwayListener>
   );
