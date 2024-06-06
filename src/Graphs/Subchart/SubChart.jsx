d146cd9042f0dbe492582534a3942b1bc8d8b40e
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2024-06-06 09:09:18 +0300

    Update SubChart.jsx

--- src/Graphs/Subchart/SubChart.jsx
+++ src/Graphs/Subchart/SubChart.jsx
@@ -328,6 +328,7 @@ export default function SubChart(props) {
     _controlWrapper = controlWrapper
   }) => {
     if (!_allInitialColumns) return;
+    if (!_chartWrapper) return;
 
     setDataColumns(newDataColumns);
 
@@ -342,7 +343,7 @@ export default function SubChart(props) {
           }; // 'hide' the serie by making it transparent
       });
 
-      _chartWrapper?.setOptions({
+      _chartWrapper.setOptions({
         ...options,
         series: {
           ...options.series,
@@ -386,7 +387,7 @@ export default function SubChart(props) {
           }
         }
       });
-      _chartWrapper?.setView({ columns: newViewColumns });
+      _chartWrapper.setView({ columns: newViewColumns });
 
       const newOptions = { ...options };
       // Preserve the initial vAxis range so that the vAxis doesn't shift based on the visible serie(s)
@@ -408,7 +409,7 @@ export default function SubChart(props) {
         selectedSeriesCount++;
       })
       newOptions.series = series;
-      _chartWrapper?.setOptions(newOptions);
+      _chartWrapper.setOptions(newOptions);
 
       if (hasChartControl) {
         const currentControlOptions = _controlWrapper?.getOptions();
@@ -430,7 +431,7 @@ export default function SubChart(props) {
     }
 
     // Call draw to apply the new DataView and 'refresh' the chart
-    _chartWrapper?.draw();
+    _chartWrapper.draw();
 
     if (hasChartControl) {
       _controlWrapper?.draw();
@@ -513,8 +514,10 @@ export default function SubChart(props) {
 
       let thisControlWrapper;
       if (hasChartControl) {
-        const thisDashboardWrapper = new google.visualization.Dashboard(
-          document.getElementById(`dashboard-${chartID}`));
+        const dashboardDOM = document.getElementById(`dashboard-${chartID}`);
+        if (!dashboardDOM) return;
+
+        const thisDashboardWrapper = new google.visualization.Dashboard(dashboardDOM);
         setDashboardWrapper(thisDashboardWrapper);
 
         google.visualization.events.addListener(thisDashboardWrapper, 'ready', onChartReady);
